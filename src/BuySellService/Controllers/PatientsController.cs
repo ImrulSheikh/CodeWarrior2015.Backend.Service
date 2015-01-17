528fcd3b43ea43d384b85c167dafb9a4742b5d44
nurulasad <nurulasad@yahoo.com>
2015-01-17 19:26:05 +0600

    Initial

---
+++ src/BuySellService/Controllers/PatientsController.cs
@@ -0,0 +1,22 @@
+﻿using PatientData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PatientData.Controllers
+{
+    public class PatientsController : ApiController
+    {
+        public PatientsController()
+        {
+
+        }
+        public List<Patient> Get()
+        {
+            return PatientDb.GetGata();
+        }
+    }
+}
---
+++ src/BuySellService/Models/PatientDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PatientData.Models
+{
+    public class PatientDb
+    {
+        public static List<Patient> GetGata()
+        {
+            var data = new List<Patient>();
+            for (int i = 0; i < 3; i++)
+            {
+                data.Add(new Patient() { Id = "id" + i, Name = "Name_" + i });
+            }
+            return data;
+
+        }
+    }
+}
