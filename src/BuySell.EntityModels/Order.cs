f9f567d8da587e18817aa78594b20d6c33e01e27
MIbnoon <mibnoon@bd.imshealth.com>
2015-01-24 02:11:56 +0600

    ProductCRUD.mdf added with context, initial migration,

---
+++ src/BuySell.EntityModels/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BuySell.EntityModels
+{
+    public class Order:BaseCoreEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+    }
+}
