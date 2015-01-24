68b708747ed0bfb2f3c5060a2bc14afd0b74ccd0
MIbnoon <mibnoon@bd.imshealth.com>
2015-01-25 01:24:34 +0600

    ProductCRUDContext, ProductFlatContext, UserStatsContext added

--- CodeWarrior2015.Backend.Service.UserStats/UserStatsContext.cs
+++ CodeWarrior2015.Backend.Service.UserStats/UserStatsContext.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CodeWarrior2015.Backend.Service.UserStats
 {
-    public class Class1
+    public class UserStatsContext : DbContext
     {
     }
 }
