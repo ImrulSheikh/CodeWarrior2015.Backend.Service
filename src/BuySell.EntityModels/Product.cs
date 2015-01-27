3b9e9da6df8e5087075a9692d369a44d6f388d54
MIbnoon <mibnoon@bd.imshealth.com>
2015-01-28 05:01:55 +0600

    ImagePath added to Product

--- src/BuySell.EntityModels/Product.cs
+++ src/BuySell.EntityModels/Product.cs
@@ -9,6 +9,7 @@ namespace BuySell.EntityModels
         public string Name { get; set; }
         public string Description { get; set; }
         public int NumberOfUnits { get; set; }
+        public string ImagePath { get; set; }
         public ICollection<ProductProperty> Properties { get; set; }
         public ICollection<ProductPrice> Prices { get; set; }
         public ICollection<ProductGroup> Groups { get; set; }
