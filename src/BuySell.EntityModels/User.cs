38adf1d86142a1d081ba396f83ee9ca68301cf14
Mufleh Ibnoon <mibnoon@bd.imshealth.com>
2015-01-27 14:21:28 +0600

    User Property Added Sex

--- src/BuySell.EntityModels/User.cs
+++ src/BuySell.EntityModels/User.cs
@@ -7,6 +7,7 @@ namespace BuySell.EntityModels
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Sex { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
         public ICollection<Product> Products { get; set; }
