0ce2037c084b72d2ba21e2e1b62866b969780745
Mufleh Ibnoon <mibnoon@bd.imshealth.com>
2015-01-28 18:52:54 +0600

    RepositoryBase, ProductRepository added

---
+++ CodeWarrior2015.Backend.Service.ProductCRUD/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuySell.EntityModels;
+using CodeWarrior2015.Backend.Service.ProductCRUD.Repositories.Interfaces;
+
+namespace CodeWarrior2015.Backend.Service.ProductCRUD.Repositories
+{
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(DbContext context) : base(context) { }
+    }
+}
---
+++ CodeWarrior2015.Backend.Service.ProductCRUD/Repositories/RepositoryBase.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using CodeWarrior2015.Backend.Service.ProductCRUD.Repositories.Interfaces;
+
+namespace CodeWarrior2015.Backend.Service.ProductCRUD.Repositories
+{
+    public class RepositoryBase<T> : IRepository<T>, IDisposable where T : class
+    {
+        private readonly DbContext _ctx;
+        public RepositoryBase(DbContext context)
+        {
+            _ctx = context;
+        }
+
+        public void Add(T entity)
+        {
+            //_ctx.Set<T>().Add(entity);
+            _ctx.Set<T>().Attach(entity);
+            _ctx.Entry(entity).State = EntityState.Added;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _ctx.Set<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return _ctx.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            _ctx.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _ctx.Entry(entity).State = EntityState.Deleted;
+        }
+
+        public void Save()
+        {
+            _ctx.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+        }
+    }
+}
