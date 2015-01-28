b8d481fc764a9b5ec31fa65b42844721e876e38e
nurulasad <nasad>
2015-01-28 14:42:32 +0600

    api updated

--- src/BuySellService/Controllers/ProfilesController.cs
+++ src/BuySellService/Controllers/ProfilesController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Data.Entity;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
 using BuySell.EntityModels;
 using EShopper.DataContexts;
 using EShopper.Models;
@@ -13,12 +16,15 @@ using System.Web.Http.Cors;
 namespace EShopper.Controllers
 {
     [Authorize]
+    [RoutePrefix("api/Profiles")]
     public class ProfilesController : ApiController
     {
         public ProfilesController()
         {
-
+            var req = Request;
         }
+    
+        [Route("GetData")]
         public HttpResponseMessage Get()
         {
             var data = new ProfileDbContext().Profiles;
@@ -28,12 +34,47 @@ namespace EShopper.Controllers
 
         }
 
-
-        public void Add(Profile customer)
+        [HttpPost]
+       [Route("AddData")]
+        public HttpResponseMessage Add(Profile customer)
         {
             var context = new ProfileDbContext();
             context.Add(customer);
 
+            var messages = new List<string>();
+            messages.Add("item added");
+           
+            return Request.CreateResponse(HttpStatusCode.OK, messages);
+        }
+
+        [HttpPost]
+        [Route("AddImage")]
+        public async Task<HttpResponseMessage> UploadImageAsync()
+        {
+            
+
+            var messages = new List<string>();
+            messages.Add("item added");
+            if (Request.Content.IsMimeMultipartContent())
+            {
+                string uploadPath = HttpContext.Current.Server.MapPath("~/uploads");
+
+                var streamProvider = new MyStreamProvider(uploadPath);
+
+                await Request.Content.ReadAsMultipartAsync(streamProvider);
+
+
+                foreach (var file in streamProvider.FileData)
+                {
+                    var fi = new FileInfo(file.LocalFileName);
+                    messages.Add("File uploaded as " + fi.FullName + " (" + fi.Length + " bytes)");
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, messages);
+            }
+            messages.Add("file not added");
+
+            return Request.CreateResponse(HttpStatusCode.OK, messages);
         }
        
     }
