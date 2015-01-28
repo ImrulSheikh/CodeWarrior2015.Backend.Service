23ae3608be56bc0dd9a8d62fd8dc7bd4559ece12
Mufleh Ibnoon <mibnoon@bd.imshealth.com>
2015-01-28 20:43:36 +0600

    ProductFlat added with migrations

---
+++ BuySell.ViewModels/ProductFlat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuySell.EntityModels;
+
+namespace BuySell.ViewModels
+{
+    public class ProductFlat : BaseCoreEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int NumberOfUnits { get; set; }
+        public string ImagePath { get; set; }
+        public string Tags { get; set; }
+        public string Comments { get; set; }
+        public string Properties { get; set; }
+        public string Prices { get; set; }
+        public int CategoryId { get; set; }
+        public string Category { get; set; }
+        public int PostedUserId { get; set; }
+        public string PostedBy { get; set; }
+    }
+}
--- BuySell.ViewModels/Properties/AssemblyInfo.cs
+++ BuySell.ViewModels/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BuySell.Repositories")]
+[assembly: AssemblyTitle("BuySell.ViewModels")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BuySell.Repositories")]
+[assembly: AssemblyProduct("BuySell.ViewModels")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d857039d-d89e-411d-973a-9378ef164201")]
+[assembly: Guid("6338c467-b9b9-4bab-947a-74420b765c06")]
 
 // Version information for an assembly consists of the following four values:
 //
