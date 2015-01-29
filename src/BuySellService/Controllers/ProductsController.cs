874ae5e23633b93e2a888ac5838949c146b9c448
nurulasad <nasad>
2015-01-29 18:45:05 +0600

    api added

---
+++ src/BuySellService/Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿using BuySell.EntityModels;
+using BuySell.GenericRepository;
+using CodeWarrior2015.Backend.Service.ProductCRUD.Repositories;
+using EShopper.DataContexts;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EShopper.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Products")]
+    public class ProductsController : ApiController
+    {
+        private ProductRepository repository;
+        public ProductsController(IRepository<Product> repository )
+        {
+            if(repository == null)
+                repository = new ProductRepository(new ProductCRUDContext());
+        }
+    
+        [Route("GetAllProduct")]
+        public HttpResponseMessage Get()
+        {
+            
+            var data = repository.GetAll();
+
+            var response = Request.CreateResponse(data);
+
+            return response;
+
+        }
+
+        [Route("GetProductById")]
+        public HttpResponseMessage GetById(string id)
+        {
+            var data = repository.GetById(int.Parse(id));
+
+            var response = Request.CreateResponse(data);
+
+            return response;
+
+        }
+
+        [HttpPost]
+       [Route("AddProduct")]
+        public HttpResponseMessage Add(Product item)
+        {
+           
+            repository.Add(item);
+            repository.Save();
+
+            var messages = new List<string>();
+            messages.Add(item.GetType().ToString()+" added");
+           
+            return Request.CreateResponse(HttpStatusCode.OK, messages);
+        }
+    }
+}
