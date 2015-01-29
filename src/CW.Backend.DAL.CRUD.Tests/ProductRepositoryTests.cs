7ff60a8e06afec527b2f65557ed5ced8cffd3d4d
Mufleh Ibnoon <mibnoon@bd.imshealth.com>
2015-01-29 16:03:44 +0600

    Sql UserName and password added

---
+++ src/CW.Backend.DAL.CRUD.Tests/ProductRepositoryTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CodeWarrior2015.Backend.Service.ProductCRUD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CW.Backend.DAL.CRUD.Tests
+{
+    [TestClass]
+    public class ProductRepositoryTests
+    {
+        private static ProductCRUDContext _context;
+
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            _context = new ProductCRUDContext();
+        }
+
+        [TestMethod]
+        public void AddProduct()
+        {
+
+        }
+    }
+}
