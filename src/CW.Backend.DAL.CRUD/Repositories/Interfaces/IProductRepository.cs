2746075569a3e91baab551ea236f464b772087ec
sheikh-imrul <sheikh.imrul@gmail.com>
2015-01-31 14:21:11 +0600

    GetMatchingNameAndCategoriesProducts added

--- src/CW.Backend.DAL.CRUD/Repositories/Interfaces/IProductRepository.cs
+++ src/CW.Backend.DAL.CRUD/Repositories/Interfaces/IProductRepository.cs
@@ -1,9 +1,11 @@
-﻿using CW.Backend.DAL.Base.Repositories;
+﻿using System.Collections.Generic;
+using CW.Backend.DAL.Base.Repositories;
 using CW.Backend.DAL.CRUD.Entities;
 
 namespace CW.Backend.DAL.CRUD.Repositories.Interfaces
 {
     public interface IProductRepository:IRepository<Product>
     {
+        IEnumerable<Product> GetMatchingNameAndCategoriesProducts(string searchKey);
     }
 }
--- src/CW.Backend.DAL.CRUD/Repositories/ProductRepository.cs
+++ src/CW.Backend.DAL.CRUD/Repositories/ProductRepository.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using CW.Backend.DAL.CRUD.Entities;
 using CW.Backend.DAL.CRUD.Repositories.Interfaces;
 using CW.Backend.DAL.Base.Repositories;
@@ -9,6 +11,11 @@ namespace CW.Backend.DAL.CRUD.Repositories
     {
         public ProductRepository(DbContext context) : base(context) { }
 
-
+        public IEnumerable<Product> GetMatchingNameAndCategoriesProducts(string searchKey) {
+            return GetAllIncluding(p => p.Category.SubCategories)
+                .Where(p => p.Name.Contains(searchKey) ||
+                            p.Category.Name.Contains(searchKey) ||
+                            p.Category.SubCategories.Any(sC => sC.Name.Contains(searchKey)));
+        }
     }
 }
